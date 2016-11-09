20cc07735cdff0ed8ba5129510ac8dcd9fcaaf5f
DESKTOP-99N58E4\Razva <razvan.comsa@digital-partner.ro>
2016-11-09 16:24:31 +0200

    grid foundation

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,15 @@
 const React = require('react');
+var {Link} = require('react-router');
 
 
 
-const Examples = () => (<h3> Examples component</h3>);
+const Examples = () => (<div>
+<h1 className="text-center"> Examples </h1> <p> few examples</p>
+<ol><li>
+<Link to='/?location=Bucharest'>Bucharest, RO</Link>
+</li>
+</ol>
+</div>
+);
 
 module.exports = Examples;
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,9 +1,16 @@
 const React = require('react');
 const ReactNav = require('ReactNav');
 
-const Main = (props) => (<div><ReactNav />         
-<h2>Main component</h2>
+const Main = (props) => (<div><ReactNav /> 
+<div className="row">
+
+<div className="columns medium-6 large-4 small-centered">
 {props.children}
+</div>
+
+</div>
+
+
   </div>);
 
 module.exports = Main;

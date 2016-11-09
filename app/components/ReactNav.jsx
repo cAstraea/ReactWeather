924ec8e236b33d6e790b508fc155b42133b923cf
DESKTOP-99N58E4\Razva <razvan.comsa@digital-partner.ro>
2016-11-09 15:58:34 +0200

    menu

--- app/components/ReactNav.jsx
+++ app/components/ReactNav.jsx
@@ -4,11 +4,48 @@ const { Link, IndexLink } = require('react-router');
 
 
 
-const ReactNav = () => (<div>
-<h2> Nav Component </h2>
-<IndexLink to="/" activeClassName="active" activeStyle={ { fontWeight: 'bold' }}> Get Weather </IndexLink>
+const ReactNav = React.createClass({
+
+    onSearch(e) {
+        e.preventDefault();        
+    },
+    render() {
+        return (
+<div className="top-bar">
+    <div className="top-bar-left">
+        <ul className="menu">
+        <li className="menu-text"> Weather App</li>
+        <li>
+        <IndexLink to="/" activeClassName="active" activeStyle={ { fontWeight: 'bold' }}> Get Weather </IndexLink>
+        </li>
+
+                <li>
 <Link to="/about" activeClassName="active" activeStyle={ { fontWeight: 'bold' }}> About </Link>
+        </li>
+
+                <li>
 <Link to="/examples" activeClassName="active" activeStyle={ { fontWeight: 'bold' }}> Examples </Link>
-</div>);
+        </li>
+
+        </ul>
+    </div>
+
+    <div className="top-bar-right">
+        <form onSubmit={this.onSearch}>
+           <ul className="menu">
+            <li>
+                <input type="search" placeholder="city" />
+            </li>
+            <li>
+            <input type="submit" className="button" />
+            </li>
+           </ul>
+        </form>
+    
+</div>
+</div>
+);
+    }
+});
 
 module.exports = ReactNav;
